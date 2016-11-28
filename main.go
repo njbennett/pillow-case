b2fcb4e982188bea36f9e29f24d89217449784bd
Natalie J. Bennett <nbennett@pivotal.io>
2016-11-28 14:09:24 -0800

    Let Write take a sheets service

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
+import sheets "google.golang.org/api/sheets/v4"
+
 func main() {}
 
-func Write(data string) error {
+func Write(data string, srv *sheets.Service) error {
 	return nil
 }
--- pillow_case_test.go
+++ pillow_case_test.go
@@ -2,14 +2,18 @@ package main_test
 
 import (
 	pillow "github.com/njbennett/pillow-case"
+	sheets "google.golang.org/api/sheets/v4"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Write", func() {
-	It("accepts a string", func() {
-		err := pillow.Write("test")
-		Expect(err).ToNot(HaveOccurred())
+	Context("when given a string and a sheets service", func() {
+		It("returns no error", func() {
+			srv := new(sheets.Service)
+			err := pillow.Write("test", srv)
+			Expect(err).ToNot(HaveOccurred())
+		})
 	})
 })
