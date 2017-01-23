bcb32c00bfafac66b69496337e26165a23c04e76
Natalie J Bennett <natalie.j.bennett@gmail.com>
2017-01-22 22:02:17 -0800

    Finish scaffolding test

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"io/ioutil"
-
+	"fmt"
 	gserviceaccount "github.com/knq/jwt/gserviceaccount"
 	"golang.org/x/net/context"
-	oauth2Google "golang.org/x/oauth2/google"
 	sheets "google.golang.org/api/sheets/v4"
+	oauth2 "golang.org/x/oauth2"
 )
 
 func main() {}
@@ -26,48 +26,35 @@ func Read(cell string) string {
 		panic(err)
 	}
 
-	config, err := oauth2Google.ConfigFromJSON(json_creds)
-	if err != nil {
-		panic(err)
-	}
-
 	service_acc, err := gserviceaccount.FromJSON(json_creds)
 	if err != nil {
 		panic(err)
 	}
 
 	var scopes string
+	scopes = "https://www.googleapis.com/auth/spreadsheets.readonly"
 	token_source, err := service_acc.TokenSource(context.Background(), scopes)
 	if err != nil {
 		panic(err)
 	}
 
-	tok, err := token_source.Token()
-	if err != nil {
-		panic(err)
-	}
-
 	ctx := context.Background()
-	client := config.Client(ctx, tok)
+	client := oauth2.NewClient(ctx, token_source)
 
 	srv, err := sheets.New(client)
 	if err != nil {
 		panic(err)
 	}
 
-	spreadsheetId := "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms"
+	spreadsheetId := "1upV1TrDjZMP4V_ARar0UfB5Yn21O4iHHWFy1c_7-4V8"
 	var readRange string
-	if cell == "A1" {
-		readRange = "Test Data!A1"
-	} else {
-		readRange = "Test Data!B2"
-	}
+	readRange = fmt.Sprintf("TestData!%s", cell)
 
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 
 	if err != nil {
 		panic(err)
 	}
-	return resp.MajorDimension
-	return "hello"
+	
+	return (resp.Values[0][0]).(string)
 }
